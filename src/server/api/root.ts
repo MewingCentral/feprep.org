eb7a66ad777d526b208aacacba25e342068bf872
Caleb Rivera <calebrossr@gmail.com>
2024-09-12 17:10:47 -0400

    chore: clean up boilerplate

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,8 @@
-import { postRouter } from "~/server/api/routers/post";
-import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +10,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  greeting: publicProcedure.query(() => "Hello, World!"),
 });
 
 // export type definition of API
