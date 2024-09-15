345253e191ef9c227018970339074b212624a79a
Caleb Rivera <calebrossr@gmail.com>
2024-09-15 03:24:21 -0400

    feat(schema): add questions and auth related tables

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,25 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
 import {
+  boolean,
   index,
+  integer,
+  numeric,
   pgTableCreator,
   serial,
+  text,
   timestamp,
+  unique,
   varchar,
 } from "drizzle-orm/pg-core";
+import {
+  DIFFICULTIES,
+  POINTS,
+  QUESTION_NUMBERS,
+  SECTIONS,
+  TOPICS,
+} from "~/lib/consts";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -18,19 +29,129 @@ import {
  */
 export const createTable = pgTableCreator((name) => `feprep.org_${name}`);
 
-export const posts = createTable(
-  "post",
+export const users = createTable(
+  "user",
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+    id: varchar("id", { length: 21 }).primaryKey(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    hashedPassword: varchar("hashed_password", { length: 255 }).notNull(),
+    isEmailVerified: boolean("is_email_verified").default(false).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at", { mode: "date" }).$onUpdate(
+      () => new Date(),
     ),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (t) => ({
+    emailIdx: index("user_email_idx").on(t.email),
+  }),
+);
+
+export const sessions = createTable(
+  "session",
+  {
+    id: varchar("id", { length: 255 }).notNull().primaryKey(),
+    userID: varchar("user_id", { length: 21 })
+      .notNull()
+      .references(() => users.id),
+    expiresAt: timestamp("expires_at", {
+      withTimezone: true,
+      mode: "date",
+    }).notNull(),
+  },
+  (t) => ({
+    userIdx: index("session_user_idx").on(t.userID),
+  }),
+);
+
+export const emailVerificationCodes = createTable(
+  "email_verification_token",
+  {
+    id: serial("id").primaryKey(),
+    userID: varchar("user_id", { length: 21 }).unique().notNull(),
+    email: varchar("email", { length: 255 }).notNull(),
+    code: varchar("code", { length: 8 }).notNull(),
+    expiresAt: timestamp("expires_at", {
+      withTimezone: true,
+      mode: "date",
+    }).notNull(),
+  },
+  (t) => ({
+    userIdx: index("email_verification_token_user_idx").on(t.userID),
+    emailIdx: index("email_verification_token_email_idx").on(t.email),
+  }),
+);
+
+export const passwordResetTokens = createTable(
+  "password_reset_token",
+  {
+    id: varchar("id", { length: 40 }).primaryKey(),
+    userId: varchar("user_id", { length: 21 }).notNull(),
+    expiresAt: timestamp("expires_at", {
+      withTimezone: true,
+      mode: "date",
+    }).notNull(),
+  },
+  (t) => ({
+    userIdx: index("password_reset_token_user_idx").on(t.userId),
+  }),
+);
+
+export const questions = createTable("question", {
+  id: varchar("id", { length: 15 }).primaryKey(),
+  title: varchar("title", {
+    length: 255,
+  }).notNull(),
+  pdf_url: text("pdf_url").notNull(),
+  average_score: numeric("average_score", { precision: 5, scale: 2 }).notNull(),
+  points: varchar("points", { length: 2, enum: POINTS }).notNull(),
+
+  section: varchar("section", { length: 50, enum: SECTIONS }).notNull(),
+  topic: varchar("topic", { length: 50, enum: TOPICS }).notNull(),
+  questionNumber: varchar("question_number", {
+    length: 1,
+    enum: QUESTION_NUMBERS,
+  }).notNull(),
+
+  easyVotes: integer("easy_votes").notNull().default(0),
+  mediumVotes: integer("medium_votes").notNull().default(0),
+  hardVotes: integer("hard_votes").notNull().default(0),
+});
+
+export const resources = createTable("resource", {
+  id: varchar("id", { length: 15 }).primaryKey(),
+  topic: varchar("topic", { length: 50, enum: TOPICS }).notNull(),
+  url: text("url").notNull(),
+  isVideo: boolean("is_video").notNull(),
+});
+
+export const comments = createTable("comment", {
+  id: varchar("id", { length: 15 }).primaryKey(),
+  questionID: varchar("question_id", { length: 15 }).notNull(),
+  userID: varchar("user_id", { length: 21 }).notNull(),
+  text: text("text").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { mode: "date" }).$onUpdate(
+    () => new Date(),
+  ),
+  parent_comment_id: varchar("parent_comment_id", { length: 15 }),
+});
+
+export const votes = createTable(
+  "vote",
+  {
+    id: varchar("id", { length: 15 }).primaryKey(),
+    userID: varchar("user_id", { length: 21 })
+      .notNull()
+      .references(() => users.id),
+    questionID: varchar("question_id", { length: 15 })
+      .notNull()
+      .references(() => questions.id),
+    vote: varchar("vote", {
+      length: 6,
+      enum: DIFFICULTIES,
+    }).notNull(),
+  },
+  (t) => ({
+    unique: unique("vote_unique_idx").on(t.userID, t.questionID),
+  }),
 );
